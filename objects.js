9e9e8a8b366b7fa9f57db2748a27e881f528eaa3
pyan83 <puyanwei@gmail.com>
2017-02-15 11:40:03 +0000

    Add files via upload

--- objects.js
+++ objects.js
@@ -1,13 +1,36 @@
-function player1() {
-	this.x = 20;
-	this.y = height / 2;
+function paddle1() {
+	this.x = 10;
+	this.y = 100;
+	this.w = 12;
+	this.h = 64;
+
 	this.ydir = 0;
-	this.r = 12;
 
 	this.show = function () {
 		fill(255);
-		rectMode(CENTER);
-		rect(this.x, this.y, this.r, this.r * 4);
+		rect(this.x, this.y, this.w, this.h);
+	}
+
+	this.setDir = function (dir) {
+		this.ydir = dir;
+	}
+
+	this.move = function (dir) {
+		this.y += this.ydir * 5;
+	}
+}
+
+function paddle2() {
+	this.x = 380;
+	this.y = 300;
+	this.w = 12;
+	this.h = 64;
+
+	this.ydir = 0;
+
+	this.show = function () {
+		fill(255);
+		rect(this.x, this.y, this.w, this.h);
 	}
 
 	this.setDir = function (dir) {
@@ -23,31 +46,32 @@ function ball() {
 	this.x = 100; //Start location of ball
 	this.y = 25;
 	this.r = 5;
+	this.xspeed = 5;
+	this.yspeed = 5;
+
 	this.show = function () {
 		fill(255);
-		ellipse(this.x, this.y, this.r * 2, this.r * 2);
+		ellipse(this.x, this.y, this.r * 2);
 	}
 
 	this.move = function () {
-		this.x = this.x + xspeed;
-		this.y = this.y + yspeed;
+		this.x = this.x + this.xspeed;
+		this.y = this.y + this.yspeed;
 	}
 
 	this.edges = function () {
 		if (this.y > height || this.y < 0) {
-			yspeed = yspeed * -1;
+			this.yspeed = this.yspeed * -1;
 		}
 		if (this.x > height || this.x < 0) {
-			xspeed = xspeed * -1;
+			this.xspeed = this.xspeed * -1;
 		}
 	}
-
-	this.intersects = function (other) {
-		var d = dist(this.x, this.y, other.x, other.y);
-		if (d < this.r + other.r) {
+	this.hit = function (other) {
+		if (this.x + this.r > other.x && this.y + this.r > other.y && this.x - this.r < other.x + other.w && this.y - this.r < other.y + other.h) {
 			return true;
 		} else {
-			return false;
+			return false
 		}
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 var p1;
-var xspeed = 6;
-var yspeed = 6;
+var p2;
 
 function setup() {
 	createCanvas(400, 400);
-	p1 = new player1();
+	p1 = new paddle1();
+	p2 = new paddle2();
 	b = new ball();
 }
 
@@ -12,14 +12,22 @@ function draw() {
 	background(51);
 	p1.show();
 	p1.move();
+	p2.show();
+	p2.move();
 	b.show();
 	b.move();
 	b.edges();
-	if (b.intersects(p1)) {
-		xspeed = xspeed * -1;
-		yspeed = yspeed * -1;
+	if (b.hit(p1)) {
+		b.xspeed = b.xspeed * -1;
+		b.yspeed = b.yspeed * -1;
+	}
+	if (b.hit(p2)) {
+		b.xspeed = b.xspeed * -1;
+		b.yspeed = b.yspeed * -1;
 	}
 }
+
+// }
 
 function keyPressed() {
 	if (keyCode === 87) {
@@ -27,8 +35,15 @@ function keyPressed() {
 	} else if (keyCode === 83) {
 		p1.setDir(1);
 	}
+	if (keyCode === UP_ARROW) {
+		p2.setDir(-1);
+	} else if (keyCode === DOWN_ARROW) {
+		p2.setDir(1);
+	}
 }
 
 function keyReleased() {
 	p1.setDir(0);
+	p2.setDir(0);
+
 }
